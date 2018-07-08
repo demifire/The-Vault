25e3ff7b965884a5792b94acdedfbe027360c51a
Chaz Kondo <Chaz@2ppgn4x.local>
2018-07-07 16:47:03 -1000

    Finished

--- vault.js
+++ vault.js
@@ -1,4 +1,24 @@
 'use strict';
 module.exports = function() {
-  
+
+    const myGlobal = {};
+
+    const setValue = function(key, value){
+        if(!(myGlobal.key)){
+            myGlobal[key] = value;
+        }
+    };
+
+    const getValue = function(key){
+        if(myGlobal[key]){
+            return myGlobal[key]
+        }else{
+            return null
+        }
+    };
+
+  return {
+      getValue: getValue,
+      setValue: setValue,
+  }
 };
